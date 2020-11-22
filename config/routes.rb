a20c669cf83d1b4209d4df62c66ebad5b7f125b2
challenger-R <65025870+challenger-R@users.noreply.github.com>
2020-11-22 23:28:17 +0900

    user
    user

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "lists#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :edit, :update]
+  resources :lists, only: [:index, :new, :create, :show]
+
 end
